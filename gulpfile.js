2c5b21d8a78068dfa6ca3500ea942732b1585dce
Daniel Perez <pereznetworks@live.com>
2018-09-02 10:47:35 -0700

    gulpfile.js working
    remove gulp-cli and gulp@3.9.1 globally -- reinstall gulp-cli and  gulp@4.0.0 -g -save-dev
    
    now need to signal for async completion and declare a few more path variables and tasks

--- gulpfile.js
+++ gulpfile.js
@@ -15,21 +15,6 @@
 // vars for src and dist folder paths
 const options = { src: 'src', dist: 'dist'};
 
-// compile sass into css,
-// copy to /dist/css folder,
-// save global.css and map.css
-// call using gulp.task('compileSass');
-
-function compileSass() {
-  return gulp.src(`${options.src}/sass/global.scss`)
-      .pipe(maps.init())
-      .pipe(sass())
-      .pipe(maps.write('./'))
-      .pipe(gulp.dest(`${options.src}/css`));
-});
-
-compileSass.discription = `using build ref, compile sass into css, a map file, copy to /dist/css folder`;
-
 // optimize and/or compress asset files, images and icons for distribution
 // hadnt actually found a gulp module for this one yet...
 // for now were just copying the files to /dist
@@ -37,11 +22,26 @@ compileSass.discription = `using build ref, compile sass into css, a map file, c
 // call using gulp.task("images");
 
 function images() {
-  return gulp.src([ options.src + '/images/*.jpg', options.src + '/images/*.png', options.src + '/icons/**'], { base: './' + options.src })
-            .pipe(gulp.dest(`${options.dist}`));
-};
+  return gulp.src([ './src/images/*.jpg', './src/images/*.png'])
+            .pipe(gulp.dest(`./dist`));
+}
 
 images.description = `optimize images files, copy to /dist folder`;
+
+// compile sass into css,
+// copy to /dist/css folder,
+// save global.css and map.css
+// call using gulp.task('compileSass');
+
+function compileSass() {
+  return gulp.src(`./src/sass/global.scss`)
+      .pipe(maps.init())
+      .pipe(sass())
+      .pipe(maps.write('./'))
+      .pipe(gulp.dest(`./dist/css`));
+}
+
+compileSass.discription = `using build ref, compile sass into css, a map file, copy to /dist/css folder`;
 
 // prep SASS files for distribution
 // run compileSass
@@ -50,28 +50,33 @@ images.description = `optimize images files, copy to /dist folder`;
 // copy to /dist folder and overwrite if any exist
 // call using gulp.task('styles',['compileSass']);
 
-function styles() {
-  gulp.src(options.src + '/index.html')
+function minifyCSS() {
+  return gulp.src('./src/index.html')
     .pipe(useref())
     .pipe(iff('*.css', csso()))
-    .pipe(gulp.dest(`${options.dist}`));
-};
+    .pipe(gulp.dest(`./dist`));
+}
 
-styles.description = `using build ref, run compileSass, map and minify, copy to /dist folder`;
+minifyCSS.description = `using build ref, run compileSass, map and minify, copy to /dist folder`;
 
+function styles(){
+  return gulp.series(compileSass, minifyCSS);
+}
+
+styles.description = `run compileSass and minifyCSS functions`;
 // prep JavaScript files for distribution
 // run compileSass
 // use build refs found in index.html
 // concat and minify all js
 // copy to /dist folder and overwrite if any exist
-gulp.task("scripts");
+// call using gulp.task("scripts");
 
 function scripts() {
-  gulp.src(options.src + '/index.html')
+  gulp.src('./src/index.html')
     .pipe(useref())
     .pipe(iff('*.js', uglify()))
-    .pipe(gulp.dest(`${options.dist}`));
-};
+    .pipe(gulp.dest(`./dist`));
+}
 
 scripts.description = `using build ref, minify, map and copy js files to /dist folder`;
 
@@ -80,31 +85,19 @@ scripts.description = `using build ref, minify, map and copy js files to /dist f
 // call using gulp.task("updateHTML");
 
 function updateHTML() {
-  return gulp.src([ options.src + '/images/index.html'], { base: './' + options.src })
-            .pipe(gulp.dest(`${options.dist}`));
-};
+  return gulp.src([ './src/images/index.html'])
+            .pipe(gulp.dest(`/dist`));
+}
 
 updateHTML.description = `update /dist version of html`;
-
-// remove the /dist folder and everything in it
-// call using gulp.task('clean');
-
-function clean() {
-  del([options.dist, options.dist + '/css/global.css*', options.dist + '/js/app*.js*']);
-};
-
-clean.description = `remove the /dist folder and everything in it`;
 
 // build task, compileSass tasks first
 // then run other tasks to prep src files for distribution
 // call using gulp.task("build",  ['clean', 'images']);
 
 function build() {
-  gulp.task('html');
-  gulp.task('scripts');
-  gulp.task('styles');
-  gulp.task('assets');
-};
+  gulp.series(clean, compileSass, styles, gulp.parallel(updateHTML, scripts, images));
+}
 
 build.description = `run all /dist prep tasks`;
 
@@ -113,21 +106,40 @@ build.description = `run all /dist prep tasks`;
 // when a change is detected run appropriate task
 // call using gulp.task('watchFiles');
 
-
 function watchFiles() {
-  gulp.watch(options.src + '/index.html', ['html']);
-  gulp.watch(options.src + '/images/**/**', ['images']);
-  gulp.watch(options.src + '/sass/**/*.scss', ['styles']);
-  gulp.watch(options.src + '/js/**/*.js', ['scripts']);
-};
+  gulp.watch('./src/index.html', updateHTML);
+  gulp.watch('./src/images/**/**', images);
+  gulp.watch('./src/sass/**/*.scss', styles);
+  gulp.watch('./src/js/**/*.js', scripts);
+}
 
 watchFiles = `watch for change to any src files, if so, run respective dist prep task`;
+
 // a server task
 // runs the build task
 // runs watchFiles task
 // start a server
 // if watchFiles runs any tasks, server should be restarted
+// TODO: place server function declaration here ...
+
+
+// remove the /dist folder and everything in it
+// call using gulp.task('clean');
+
+function clean() {
+  return del([`./dist`]);
+}
+
+clean.description = `remove the /dist folder and everything in it`;
+
+// declaring tasks that can called from the commandline
+// usage: gulp exportedFunctionName
+exports.clean = clean;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watchFiles = watchFiles;
+exports.build = build;
 
 // default task
-// and then run serve task
-gulp.task("default", ["build"]);
+// TODO: for now is the build task, but will be server live-reload task
+gulp.task("default", build);
